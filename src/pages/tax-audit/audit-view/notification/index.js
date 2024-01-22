354871ff167b9b22c44ec2db224232c418a15b69
prinxard <prinxard@gmail.com>
2024-01-22 10:55:45 +0100

    added email button

--- src/pages/tax-audit/audit-view/notification/index.js
+++ src/pages/tax-audit/audit-view/notification/index.js
@@ -289,6 +289,7 @@ const Notification = () => {
                     <div className="flex">
                         <button onClick={() => router.back()} className="p-2 bg-gray-400 text-white w-20 rounded mr-3">Back</button>
                         <button><a href={`https://test.rhm.backend.bespoque.ng/notification-file-pdf.php?fileno=${notice?.notification_fileno}&action=DOWNLOAD`} rel="noreferrer" target="_blank" className="p-2 bg-pink-400 text-white rounded">View letter</a></button>
+                        <button><a href={`https://test.rhm.backend.bespoque.ng/notification-file-pdf.php?fileno=${notice?.notification_fileno}&action=EMAIL`} rel="noreferrer" target="_blank" className="p-2 bg-purple-400 ml-3 text-white rounded">Email</a></button>
                     </div>
                 </div>
                 <div className="flex justify-end gap-2 items-center mb-4">
