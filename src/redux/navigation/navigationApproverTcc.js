3d220d6d0c3291fdb1c77c3a6a6b2de289662cfa
prinxard <prinxard@gmail.com>
2024-02-12 00:01:43 +0100

    update

--- src/redux/navigation/navigationApproverTcc.js
+++ src/redux/navigation/navigationApproverTcc.js
@@ -1,4 +1,4 @@
-import { RhmDashboard, ManageTaxpayer, Paye, Collections, UserGuide } from '../../components/Icons/index';
+import { RhmDashboard, ManageTaxpayer, Paye, Collections } from '../../components/Icons/index';
 
 
 const initialState = [
