a92991a3b81545adef1d82e336972ab877db3be1
prinxard <prinxard@gmail.com>
2024-12-19 16:33:26 +0100

    update

--- src/config/url.js
+++ src/config/url.js
@@ -4,6 +4,7 @@ const url = {
   // BASE_URL: "https://dev.rhmapi.irs.kg.gov.ng/api/v1/",
   // Live db
   // BASE_URL: "https://rhmapi2.irs.kg.gov.ng/api/v1/",
+  
   BASE_URL: "http://rhmapi.irs.kg.gov.ng/api/v1/",
 
 
